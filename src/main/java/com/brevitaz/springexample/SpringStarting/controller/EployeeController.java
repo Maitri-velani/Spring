c0c96183857b8afd88534a6523776166a146d5bf
mvelani <maitrivelani999@gmail.com>
2018-02-02 10:12:38 +0530

    Commit Employee class

---
+++ src/main/java/com/brevitaz/springexample/SpringStarting/controller/EployeeController.java
@@ -0,0 +1,13 @@
+package com.brevitaz.springexample.SpringStarting.controller;
+
+public class EployeeController {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
